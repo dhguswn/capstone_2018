eb8c572871eee43ef8107f56ea8f9a273f723073
dhguswn <30751310+dhguswn@users.noreply.github.com>
2018-07-20 16:22:45 +0900

    Add files via upload

---
+++ sketch_jul20a_practice.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>
+
+int servoPin=5;
+int angle=0;
+
+Servo servo1;
+
+void setup() {
+  servo1.attach(servoPin);
+  int angle=0;
+}
+
+void loop() {
+  for(angle=0;angle<180;angle++){
+    servo1.write(angle);
+    delay(5);
+    }
+  for(angle=180;angle>0;angle--){
+    servo1.write(angle);
+    delay(5);
+    }
+  delay(400);
+}
